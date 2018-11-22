e930e497eef182142e582d9da813534e55d2e54a
Rafael Scatizzi <rafaelzero01@gmail.com>
2018-11-22 09:28:39 -0500

    Bloque Try Catch

--- AppVectorea/AppVectorea/Program.cs
+++ AppVectorea/AppVectorea/Program.cs
@@ -12,7 +12,41 @@ namespace AppVectorea
         {
             Console.WriteLine("Control de versiones");
             Console.WriteLine("Control de versiones1");
+            Console.WriteLine("3 Elevado a la 4 es igual a: {0}", potencia(3,4));
+            try {
+                Console.WriteLine("-5 Elevado a la 2 es  igual a: {0}", potencia2(-5, 2));
+            } catch(Exception ex)
+            {
+                Console.WriteLine("Error! "+ex.Message.ToString());
+            }
+            Console.WriteLine("2 Elevado a la 5 es  igual a: {0}", potencia2(2, 5));
             Console.ReadKey();
+            }
+        public static double potencia (double x, int y)
+        {
+            double p = 1;
+            for (int i = 1; i <= y; i++) 
+            {
+                p = p * x;
+            }
+            return p;
+        }
+        public static double potencia2(double x, int y)
+        {
+            if (x <= 0)
+            {
+                throw new Exception("X debe ser positivo!");
+            }
+            if (y <= 0)
+            {
+                throw new Exception("Y debe ser positivo!");
+            }
+            double p = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                p = p * x;
+            }
+            return p;
         }
     }
 }
