c064f3c270cd48108ca4eb46a617cddea4d63d85
Rafael Scatizzi <rafaelzero01@gmail.com>
2018-12-03 08:06:40 -0500

    Lectura de vector Suma de Vector Promedio Vector

--- AppVectorea/AppVectorea/Program.cs
+++ AppVectorea/AppVectorea/Program.cs
@@ -10,6 +10,7 @@ namespace AppVectorea
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\tRafael Scatizzi\n********************************\n");
             Console.WriteLine("Control de versiones");
             Console.WriteLine("Control de versiones1");
             Console.WriteLine("3 Elevado a la 4 es igual a: {0}", potencia(3,4));
@@ -20,6 +21,7 @@ namespace AppVectorea
                 Console.WriteLine("Error! "+ex.Message.ToString());
             }
             Console.WriteLine("2 Elevado a la 5 es  igual a: {0}", potencia2(2, 5));
+            vectorinicial();
             Console.ReadKey();
             }
         public static double potencia (double x, int y)
@@ -47,6 +49,33 @@ namespace AppVectorea
                 p = p * x;
             }
             return p;
+        }
+        public static void vectorinicial()
+        {
+            int[] x = new int[8];
+            x[0] = 10;
+            x[1] = 2;
+            x[2] = 20;
+            x[3] = 8;
+            x[4] = 15;
+            x[5] = 1;
+            x[6] = -9;
+            x[7] = 14;
+            //imprimir el vector
+            Console.WriteLine("\n\tElementos del Vector\n====================================\n");
+            for(int i=0;i<8;i++)
+            {
+                Console.WriteLine("Elemento indice {0} --> {1}", (i + 1), x[i]);
+            }
+            //suma de los elementos vector
+            int s = 0;
+            Console.WriteLine("\n\tElementos del Vector\n===================================\n");
+            for (int i = 0; i < x.Length; i++)
+            {
+                s = s + x[i];
+            }
+            Console.WriteLine("La sumatoria es: {0}",s);
+            Console.WriteLine("El promedio es: {0}", s/x.Length);
         }
     }
 }
